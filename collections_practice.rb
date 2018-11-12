0f4d00e4fbedc56cf9e8e0abfd9344dd6632cb07
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-12 19:11:02 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
-# your code goes here
+def begins_with_r(array)
+array.each { |string|
+    return false if string.start_with?("r") == false }
+array.each { |string|
+    return true if string.start_with?("r") == true }
+end
+
+def contain_a(array)
+ array.select { |element| element.include?('a') }
+end
+
+def first_wa(array)
+  array.find{ |element| element.start_with?('wa') }
+end
